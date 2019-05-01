0038977226100b62081cf664fbb95e3c9c747317
Anrijs Gedroics <anrijsgedroics@gmail.com>
2019-05-01 18:08:05 +0300

    UI based updates

--- forum/admin.py
+++ forum/admin.py
@@ -1,7 +1,10 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
 
-from .models import User, Section
+from .models import User, Section, Thread, Post
 
 admin.site.register(User, UserAdmin)
 admin.site.register(Section)
+admin.site.register(Thread)
+admin.site.register(Post)
+
--- forum/views.py
+++ forum/views.py
@@ -1,5 +1,19 @@
+from django.core import serializers
+from django.http import HttpResponse
 from django.shortcuts import render
 
+from .models import Section, Thread
+
+
 def index(request):
-    return render(request, 'index.html', {'a':0})
+    section_list = Section.objects.all();
+    thread_list = Thread.objects.all();
+    return render(request, 'index.html', {'sections':section_list, 'threads':thread_list})
+
+
+def getThreads(request, section_id):
+    limit = int(request.GET.get('limit', '20'))
+    offset = int(request.GET.get('offset', '0'))
+    threads = Thread.objects.filter(section=int(section_id))[offset:offset+limit]
+    return HttpResponse(serializers.serialize('json', threads), content_type="application/json")
 
